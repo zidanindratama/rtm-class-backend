a956ca49ef5c071e445a2b5f96d01985d8488715
nbintang <nbintangh@gmail.com>
2026-03-05 17:43:49 +0700

    feat: use fastify

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { configureApp } from './bootstrap';
 import { configureSwagger } from './swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   configureApp(app);
   configureSwagger(app);
 
   const port = Number(process.env.PORT ?? 5000);
   await app.listen(port, '0.0.0.0');
   Logger.log(
-    `SmartClass backend is running on http://localhost:${port}/api/v1`,
+    `RTM Class backend is running on http://localhost:${port}/api/v1`,
     'Bootstrap',
   );
   Logger.log(
